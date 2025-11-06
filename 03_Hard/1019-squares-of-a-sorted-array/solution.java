39a4e7cfb7968f6db4638a123db04ac9a2373767
srikar-up <srikarsensai@gmail.com>
2025-11-06 17:03:19 +0000

    [Hard Sync] Runtime - 657 ms (5.04%), Memory - 47.5 MB (34.96%)

---
+++ 03_Hard/1019-squares-of-a-sorted-array/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n= nums.length;
+        for(int i = 0;i<n;i++){
+            nums[i] = (int)Math.pow(nums[i],2);
+        }
+        
+        for(int i = 0;i<n;i++){
+          for(int j = 0;j<n-1;j++){
+            if(nums[j]>nums[j+1]){
+                int temp = nums[j];
+                nums[j]=nums[j+1];
+                nums[j+1]=temp;
+            }
+}
+            }
+        return nums;
+    }
+}
